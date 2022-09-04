1457fe098bddd54ea94964ffd0a1656737b86677
gexgener1l <77413669+gexgener1l@users.noreply.github.com>
2022-09-04 16:25:53 +0300

    push the project

---
+++ server/index.js
@@ -0,0 +1,36 @@
+require("dotenv").config()
+const express = require("express");
+const sequelize = require("./db")
+const modules = require("./models/models")
+const cors = require("cors")
+const fileupload = require("express-fileupload")
+const router = require("./routes/index")
+const PORT = process.env.PORT || 3000 // 5000
+const errorHandler = require("./middleware/ErrorHandlingMiddleware")
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+app.use(fileupload({}))
+app.use("/api", router)
+
+//Обработка ошибок, последний Middleware
+app.use(errorHandler)
+// app.get("/",(req, res)=>{
+//     res.status(200).json({message:"WORKING!!!"})
+// })
+
+
+//стартуем сервер я ебал собак
+const start = async () => {
+    try{
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT,()=>console.log(`"server started on port ${PORT}"`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+start()
+
+
