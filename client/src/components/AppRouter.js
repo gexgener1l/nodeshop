a35ad8108c10bb610750b37f3cd19c258cfbd7c1
Дмитрий Василевич <gexg.work@gmail.com>
2022-09-24 06:28:28 +0300

    33

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import {Switch,Route,Redirect} from "react-router-dom";
+import React, {useContext} from 'react';
+import {Routes,Route} from "react-router-dom";
+import {authRoutes, publicRoutes} from "../routes";
+import Shop from "../pages/Shop";
+import {Context} from "../index";
 
 const AppRouter = () => {
-    return (
-        <div>
+    const {user} = useContext(Context)
 
-        </div>
+    console.log(user)
+    return (
+        <Routes>
+            {user.isAuth && authRoutes.map(({path, Component})=>
+                <Route key={path} path={path} element ={<Component/>} exact/>
+            )}
+            {publicRoutes.map(({path, Component})=>
+                <Route key={path} path={path} element ={<Component/>} exact/>
+            )}
+            <Route path="*" element={<Shop/>}/>
+        </Routes>
     );
 };
 
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import {Button, Card, Container, Form, FormControl, Row} from "react-bootstrap";
+import {NavLink,useLocation} from "react-router-dom";
+import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils/consts";
 
 const Auth = () => {
+    const location = useLocation()
+    const isLogin = location.pathname === LOGIN_ROUTE
     return (
-        <div>
-            Auth
-        </div>
+        <Container className={"d-flex justify-content-center align-items-center"}
+        style={{height:window.innerHeight - 54}}>
+            <Card style={{width: 600}} className={"p-5"}>
+                <h2 className={"m-lg-auto"}>{isLogin ? "Авторизация" : "Регистрация"}</h2>
+                <Form className={"d-flex flex-column"}>
+                    <FormControl
+                    placeholder="Введите email"
+                    />
+                    <FormControl
+                        placeholder="Введите password"
+                    />
+                    <Row className={"d-flex justify-content-between mt-3"}>
+                        {isLogin?
+                            <div>
+                                Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зайти</NavLink>
+                            </div>
+                            :
+                            <div>
+                                Есть аккаунта? <NavLink to={LOGIN_ROUTE}>войти</NavLink>
+                            </div>
+                        }
+
+                        <Button
+                            variant={"outline-success"}>
+                            {isLogin? 'войти':"Рагистрация"}
+                        </Button>
+                    </Row>
+
+                </Form>
+            </Card>
+        </Container>
     );
 };
 
