34754d92fb668071c9a02f21c0a75c27f8b241d5
gexgener1l <77413669+gexgener1l@users.noreply.github.com>
2022-09-24 06:47:10 +0300

    fgh
    fgh

---
+++ client/src/components/DeviceItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Col} from "react-bootstrap";
+
+const DeviceItem = ({device}) => {
+    return (
+        <Col md={3}>
+
+        </Col>
+    );
+};
+
+export default DeviceItem;
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React, {useContext} from 'react';
+import {Context} from "../index";
+import {Button, Container, Nav, Navbar} from "react-bootstrap";
+import {NavLink} from "react-router-dom";
+import {SHOP_ROUTE} from "../utils/consts";
+import {observer} from "mobx-react-lite";
+
+const NavBar = observer(() => {
+    const {user} = useContext(Context)
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <NavLink style={{color:"white"}} to={SHOP_ROUTE}>shop</NavLink>
+
+                    {user.isAuth ?
+                        <Nav className="m-0" style={{color:"white"}}>
+                        <Button variant={"outline-light"}>Выйти</Button>
+                        <Button variant={"outline-light"} className={"m-lg-2"}>Админ панель</Button>
+                        </Nav>
+                    :
+                        <Nav className="m-lg-auto" style={{color:"white"}}>
+                            <Button variant={"outline-light"} onlick={()=>user.setIsAuth(true)}>Авторизация</Button>
+
+                        </Nav>
+                    }
+            </Container>
+        </Navbar>
+    );
+});
+
+export default NavBar;
